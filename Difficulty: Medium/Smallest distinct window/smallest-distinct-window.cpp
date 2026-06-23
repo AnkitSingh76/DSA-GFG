f48d405c5db3db8d0445413dbcabebc0ef4889bc
Ankit Singh <125785064+AnkitSingh76@users.noreply.github.com>
2026-06-23 10:14:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int findSubString(string& str) {
+        vector<int> count(256, 0);
+        int first = 0, second = 0;
+        int len = str.size(), diff = 0;
+
+        while (first < str.size()) {
+            if (count[str[first]] == 0)
+                diff++;
+
+            count[str[first]]++;
+            first++;
+        }
+
+        for (int i = 0; i < 256; i++)
+            count[i] = 0;
+
+        first = 0;
+
+        while (second < str.size()) {
+
+            while (diff && second < str.size()) {
+                if (count[str[second]] == 0)
+                    diff--;
+
+                count[str[second]]++;
+                second++;
+            }
+
+            while (diff == 0) {
+                len = min(len, second - first);
+
+                count[str[first]]--;
+
+                if (count[str[first]] == 0)
+                    diff++;
+
+                first++;
+            }
+        }
+
+        return len;
+    }
+};
